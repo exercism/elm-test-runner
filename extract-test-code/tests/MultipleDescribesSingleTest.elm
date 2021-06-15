2fe4d7d55b35835ac98b304af93f0797552b3edc
Cedd Burge <ceddlyburge@gmail.com>
2021-06-15 20:19:31 +0100

    Add tests

---
+++ extract-test-code/tests/MultipleDescribesSingleTest.elm
@@ -0,0 +1,57 @@
+module MultipleDescribesSingleTest exposing (..)
+
+import Expect exposing (Expectation)
+import ExtractTestCode
+import Test exposing (..)
+
+
+tests : Test
+tests =
+    test "Can extract single test in describe wrapper" <|
+        \_ ->
+            """module AnnalynsInfiltrationTests exposing (tests)
+
+import Test exposing (..)
+
+tests : Test
+tests =
+    describe "AnnalynsInfiltration"
+        [ describe "AnnalynsInfiltration task 1"
+            [ test "Cannot execute fast attack if knight is awake" <|
+            \\_ ->
+                let
+                    knightIsAwake =
+                        True
+                in
+                canFastAttack knightIsAwake
+                    |> Expect.equal False
+            ]
+        , describe "AnnalynsInfiltration task 2"
+            [ test "Can execute fast attack if knight is sleeping" <|
+                \\_ ->
+                    let
+                        knightIsAwake =
+                            False
+                    in
+                    canFastAttack knightIsAwake
+                        |> Expect.equal True
+            ]
+        ]
+"""
+                |> ExtractTestCode.extractTestCode
+                |> Expect.equal """Cannot execute fast attack if knight is awake
+let
+  
+  
+  knightIsAwake  =
+      True
+in
+  canFastAttack knightIsAwake |> Expect.equal False
+Can execute fast attack if knight is sleeping
+let
+  
+  
+  knightIsAwake  =
+      False
+in
+  canFastAttack knightIsAwake |> Expect.equal True"""
---
+++ extract-test-code/tests/SingleDescribeSingleTest.elm
@@ -0,0 +1,53 @@
+module SingleDescribeSingleTest exposing (..)
+
+import Expect exposing (Expectation)
+import ExtractTestCode
+import Test exposing (..)
+
+
+tests : Test
+tests =
+    test "Can extract single test in describe wrapper" <|
+        \_ ->
+            """module AnnalynsInfiltrationTests exposing (tests)
+
+import Test exposing (..)
+
+tests : Test
+tests =
+    describe "AnnalynsInfiltration"
+        [ test "Cannot execute fast attack if knight is awake" <|
+            \\_ ->
+                let
+                    knightIsAwake =
+                        True
+                in
+                canFastAttack knightIsAwake
+                    |> Expect.equal False
+        , test "Can execute fast attack if knight is sleeping" <|
+            \\_ ->
+                let
+                    knightIsAwake =
+                        False
+                in
+                canFastAttack knightIsAwake
+                    |> Expect.equal True
+        ]
+"""
+                |> ExtractTestCode.extractTestCode
+                |> Expect.equal """Cannot execute fast attack if knight is awake
+let
+  
+  
+  knightIsAwake  =
+      True
+in
+  canFastAttack knightIsAwake |> Expect.equal False
+Can execute fast attack if knight is sleeping
+let
+  
+  
+  knightIsAwake  =
+      False
+in
+  canFastAttack knightIsAwake |> Expect.equal True"""
