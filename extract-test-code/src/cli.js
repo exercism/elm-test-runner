bed6df74b5adf5975d0db5173cf0500bec1d00c5
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-06-19 20:36:31 +0200

    Replace process.stdin.fd by 0 because of errors
    I observed weird errors saying:
    
    > Error: EAGAIN: resource temporarily unavailable
    
    And apparently, it has to do with the `process.stdin.fd` getter
    according to this SO answer:
    https://stackoverflow.com/questions/40362369/stdin-read-fails-on-some-input
    
    So I just replaced it with 0.

--- extract-test-code/src/cli.js
+++ extract-test-code/src/cli.js
@@ -1,11 +1,10 @@
-
 // Link to compiled Elm code main.js
-var Elm = require('./main').Elm;
+var Elm = require("./main").Elm;
 var main = Elm.Main.init();
 
 // Get data from stdin
 var fs = require("fs");
-var input = fs.readFileSync(process.stdin.fd, "utf-8");
+var input = fs.readFileSync(0, "utf-8"); // 0 is stdin
 //console.log("\n   Input: ", input)
 
 // Send data to the elm app
@@ -14,6 +13,5 @@ main.ports.get.send(input);
 // Get data from the elm app
 main.ports.put.subscribe(function (output) {
   // send normalized code to stdout
-  console.log(output)
+  console.log(output);
 });
-
