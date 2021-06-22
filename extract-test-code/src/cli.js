8e4ab6fc0b155084a2bcfa0ffa368810b8032a48
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-06-22 21:58:26 +0200

    Fix forgotten port rename on JS side

--- extract-test-code/src/cli.js
+++ extract-test-code/src/cli.js
@@ -8,10 +8,10 @@ var input = fs.readFileSync(0, "utf-8"); // 0 is stdin
 //console.log("\n   Input: ", input)
 
 // Send data to the elm app
-main.ports.get.send(input);
+main.ports.stdin.send(input);
 
 // Get data from the elm app
-main.ports.put.subscribe(function (output) {
+main.ports.stdout.subscribe(function (output) {
   // send normalized code to stdout
   console.log(output);
 });
