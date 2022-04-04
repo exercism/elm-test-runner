d60edb9258a41c7e43fcf168cbfb927c2807285d
Jérémie Gillet <jie.gillet@gmail.com>
2022-04-05 00:00:17 +0900

    Simplify and merge smoke test scripts

--- bin/smoke_test.sh
+++ bin/smoke_test.sh
@@ -6,13 +6,31 @@ set -u # Exit on usage of undeclared variable.
 set -o pipefail # Catch failures in pipes.
 
 for solution in test_data/*/* ; do
-  echo $solution
   slug=$(basename $(dirname $solution))
   solution=$(realpath $solution)
+
   # run tests
   bin/run.sh $slug $solution $solution > /dev/null
+
   # check result
-  bin/check_files.sh $solution
+  if [[ ! -f "${solution}/expected_results.json" ]]; then
+    echo "🔥 ${solution}: expected expected_results.json to exist 🔥"
+    exit 1
+  fi
+
+  if [[ ! -f "${solution}/results.json" ]]; then
+    echo "🔥 ${solution}: expected results.json to exist on successful run 🔥"
+    exit 1
+  fi
+
+  jq -S . ${solution}/expected_results.json > /tmp/expected.json
+  jq -S . ${solution}/results.json > /tmp/actual.json
+  if ! diff /tmp/expected.json /tmp/actual.json ;then
+    echo "🔥 ${solution}: expected results.json to equal expected_results.json on successful run 🔥"
+    exit 1
+  fi
+
+  echo "🏁 ${solution}: expected files present and correct after successful run 🏁"
 done
 
 echo "Smoke test finished"
